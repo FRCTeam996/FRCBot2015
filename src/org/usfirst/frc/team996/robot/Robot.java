d37483bad231664eb0f56dcce0565430937a2785
koubmi <bouk555@gmail.com>
2015-01-24 12:17:57 -0700

    More Clean-Up and Axis Camera Control
    Cleaned up code further. Moved hard-coded vars to final vars at top of
    the Robot class. Axis camera movement added.

--- src/org/usfirst/frc/team996/robot/Robot.java
+++ src/org/usfirst/frc/team996/robot/Robot.java
@@ -3,9 +3,7 @@ package org.usfirst.frc.team996.robot;
 
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.Image;
-
 import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -23,13 +21,27 @@ import edu.wpi.first.wpilibj.vision.AxisCamera;
  * Begining of Camera Vision
  * Axis Camera Control with servos
  * 
+ * Known Bugs:
+ * Camera Jerks when tele-OP started
  * 
  */
 
 public class Robot extends SampleRobot {
 	
 	//Final Vars
-	final double DEFAULT_DELAY = 0.05;
+	final double DEFAULT_DELAY = 0.005; //Delay for the main loop
+	final double ROBOT_EXPIRATION_TIME = 0.1;
+	
+	//Axis Camera Settings
+	final double CAMERA_Y_DEADZONE = 0.01;
+	final double CAMERA_X_DEADZONE = 0.01;
+	final String AXIS_CAM_IP = "10.9.96.11";
+	
+	//PWM Channels
+	final int LEFT_TALON_PWM_PIN = 0;
+	final int RIGHT_TALON_PWM_PIN = 1;
+	final int CAMERA_Y_AXIS_PWM_PIN = 8;
+	final int CAMERA_X_AXIS_PWM_PIN = 9;
 	
 	//Chassis
     RobotDrive myRobot;
@@ -55,25 +67,25 @@ public class Robot extends SampleRobot {
     	
     	//Chassis
     	try{
-    		leftMotorControl = new Talon(0);
-    		rightMotorControl = new Talon(1);
+    		leftMotorControl = new Talon(LEFT_TALON_PWM_PIN);
+    		rightMotorControl = new Talon(RIGHT_TALON_PWM_PIN);
     	}catch(Exception e){
     		System.out.println("Error with chassis talons");
     	}
         myRobot = new RobotDrive(leftMotorControl, rightMotorControl);
-        //Need to set expiration here?
+        myRobot.setExpiration(ROBOT_EXPIRATION_TIME);
         
         //Driver Station
         stick = new Joystick(0);
         cStick = new Joystick(1);
         
         //Axis Camera
-        axisCam = new AxisCamera("10.9.96.11");
+        axisCam = new AxisCamera(AXIS_CAM_IP);
         axisCam.writeResolution(AxisCamera.Resolution.k320x240);
         axisCam.writeBrightness(0);
         try{
-        	cameraYServo = new Servo(8);
-        	cameraXServo = new Servo(9);
+        	cameraYServo = new Servo(CAMERA_Y_AXIS_PWM_PIN);
+        	cameraXServo = new Servo(CAMERA_X_AXIS_PWM_PIN);
         }catch(Exception e){
         	System.out.println("[!] Error with Axis Camera servos\nTry checking pwm channels.");
         }
@@ -82,9 +94,11 @@ public class Robot extends SampleRobot {
         soundIn = new AnalogInput(2);
         
         //Computer Vision
+        /*
         image = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
         session = NIVision.IMAQdxOpenCamera("cam0",NIVision.IMAQdxCameraControlMode.CameraControlModeController);
-        NIVision.IMAQdxConfigureGrab(session);
+        NIVision.IMAQdxConfigureGrab(session)
+        */
     }
 
     public void autonomous() {
@@ -97,7 +111,6 @@ public class Robot extends SampleRobot {
         myRobot.setSafetyEnabled(true);
         
         //Driving Loop
-        int old = 0;
         while (isOperatorControl() && isEnabled()) {
         	
         	//Set "delay" to the default value
@@ -115,6 +128,12 @@ public class Robot extends SampleRobot {
         	}
         	
         	//Move Axis Camera
+        	if(Math.abs(cStick.getY()) > CAMERA_Y_DEADZONE){
+        		cameraYServo.setAngle(cameraYServo.getAngle() + cStick.getY());	
+        	}
+        	if(Math.abs(cStick.getX()) > CAMERA_X_DEADZONE){
+        		cameraXServo.setAngle(cameraXServo.getAngle() + cStick.getX());
+        	}
         	
         	//Wait for x secs
         	Timer.delay(delay);
