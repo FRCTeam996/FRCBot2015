49d47d09ad6ae55d0245afd5f1fc015a1bddb7d2
koubmi <bouk555@gmail.com>
2015-02-14 00:05:11 -0700

    Claw Added
    *Claw created
    *Basic Template created for both single and double solenoids since I
    have zero idea which one we have and zero time to do it tomorrow.

--- src/org/usfirst/frc/team996/robot/Claw.java
+++ src/org/usfirst/frc/team996/robot/Claw.java
@@ -1,16 +1,55 @@
 package org.usfirst.frc.team996.robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
+
 public class Claw {
 	
-	/*
-	 * Inputs:
-	 * Joysticks
-	 * UltraSonic
-	 * 
-	 * Solenoids
-	 * Get UltraSonic Input
-	 * Control Claw
-	 * 
-	 */
+	//No idea if it is a double or single solenoid so... time to code for both!
+	boolean isDouble = false;
+	
+	//Solenoid
+	Solenoid solenoid;
+	DoubleSolenoid dSolenoid;
+	boolean actuate = false;
+	final int SOLENOID_PORT_1 = 1;
+	final int SOLENOID_PORT_2 = 2;
+	
+	public Claw(){
+		
+		//Single
+		solenoid = new Solenoid(SOLENOID_PORT_1);
+		
+		//Double
+		dSolenoid = new DoubleSolenoid(SOLENOID_PORT_1, SOLENOID_PORT_2);
+	}
+	
+	//Will be used later to determine whether or not enable or disable solenoid
+	public boolean clawUpdate(){
+		
+		return false;
+	}
+	
+	//Enables Solenoid
+	public boolean enableSolenoid(){
+		if(isDouble){
+			dSolenoid.set(DoubleSolenoid.Value.kForward);
+			return true;
+		}else{
+			solenoid.set(true);
+			return true;
+		}
+	}
+	
+	//Disables Solenoid
+	public boolean disableSolenoid(){
+		if(isDouble){
+			dSolenoid.set(DoubleSolenoid.Value.kReverse);
+			return true;
+		}else{
+			solenoid.set(false);
+			return true;
+		}
+	}
 
 }
